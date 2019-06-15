208ab012dc63239228ef3599a24f9cce3bc3b85a
cdbm <cdbm@cin.ufpe.br>
2019-06-15 20:19:42 -0300

    [Finishes #36, #37] restrição monitor e acesso professor

--- tests-acceptance/features/feedback.feature
+++ tests-acceptance/features/feedback.feature
@@ -18,3 +18,27 @@ Given o "feedback" atribuído para essa submissão não é realizado
 When Eu salvo o feedback
 Then a mensagem de erro "Por favor, insira um feedback" é exibida 
 Then o feedback não é postado
+
+
+Feature: As monitor,
+	I want to ver os feedbacks feitos por mim,
+	so Eu possa ver soluções e notas passadas.
+
+Scenario: restrição monitor:
+Given o campo acesso não está marcado
+And o Eu realizou o feedback da submissão 1 de Maria
+When Eu abro a tela de “Feedbacks”.
+Then  Eu posso visualizar apenas o feedback da “submissão 1” do aluna Maria realizado por mim
+
+
+Feature: Ver todos os feedbacks.
+As Professor/Monitor chefe,
+	I Want to ver os feedbacks de todos os monitores, 
+	so Eu possa ver se os feedbacks estão sendo feitos corretamente.
+
+Scenario: acesso professor:
+Given o campo acesso está marcado
+And o monitor Mateus realizou o feedback da “submissão 1” da aluna “Maria”
+And o monitor André realizou o feedback da “submissão 1” do aluno “Manoel”
+When Eu abro a tela de “Feedbacks”.
+Then  Eu posso visualizar todos feedback realizados por todos os monitores
--- tests-acceptance/typeScript/stepdefinitions/feedback.ts
+++ tests-acceptance/typeScript/stepdefinitions/feedback.ts
@@ -7,10 +7,10 @@ let expect = chai.expect;
 let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
 
 defineSupportCode(function ({ Given, When, Then }) {
-    Given(/^Eu estou na página de "([^\"]*)" de "([^\"]*)"$/, async () => {
+    Given(/^Eu estou na página de "([^\"]*)" de "([^\"]*)"$/, async (sub, aluno) => {
         await browser.get("http://localhost:4200/");
-        await $("a[name='fb']").click;
-        await $("a[name='alunoNota']").click;
+        await $("a[name='fb']").click();
+        await $("a[name='alunoNota']").click();
     });
 
     Given(/^a nota atribuída para essa submissão é "([^\"]*)"$/, async (nota) => {
@@ -19,25 +19,24 @@ defineSupportCode(function ({ Given, When, Then }) {
 
 
     When(/^Eu salvo o feedback$/, async () => {
-       await $("button[name='atribuir']").click;
+       await $("button[name='atribuir']").click();
     });
 
 
     Then(/^eu estou na página de "Submissões"$/, async () => {
-        await $("a[name='feedback']").click;
+        await $("a[name='feedback']").click();
      });
+
      Then(/^eu vejo na submissão 2 de Maria a nota "([^\"]*)" atribuída. $/, async (nota) => {
         var n = document.getElementById('aNota').nodeValue;
         await n == nota;
-     });  
-});
-
-
-defineSupportCode(function ({ Given, When, Then }) {
-    Given(/^Eu esto de novo na página de "([^\"]*)" de "([^\"]*)"$/, async () => {
+        
+     });
+     
+     Given(/^Eu esto de novo na página de "([^\"]*)" de "([^\"]*)"$/, async (sub, aluno) => {
         await browser.get("http://localhost:4200/");
-        await $("a[name='fb']").click;
-        await $("a[name='alunoNota']").click;
+        await $("a[name='fb']").click();
+        await $("a[name='alunoNota']").click();
     });
 
     Given(/^o feedback atribuído para essa submissão não é realizado$/, async () => {
@@ -45,7 +44,7 @@ defineSupportCode(function ({ Given, When, Then }) {
     });
 
     When(/^Eu salvo o feedback$/, async () => {
-       await $("button[name='atribuir']").click;
+       await $("button[name='atribuir']").click();
     });
 
 
@@ -55,8 +54,46 @@ defineSupportCode(function ({ Given, When, Then }) {
      });
      
      Then(/^o feedback não é postado$/, async () => {
-        await $("a[name='feedback']").click;
+        await $("a[name='feedback']").click();
         var n = document.getElementById('aFeed').nodeValue;
         await n == "";
      });  
+
+
+     Given(/^o campo acesso não está marcado$/, async () => {
+        await browser.get("http://localhost:4200/");
+        await $("a[name='fb']").click();
+    });
+
+    Given(/^And o Eu realizou o feedback da submissão 1 de Maria$/, async () => {
+        await $("button[name='gerar']").click();
+    });
+    When(/^Eu abro a tela de Feedbacks$/, async () => {
+   
+    });
+
+    Then(/^Eu posso visualizar apenas o feedback da submissão 1 da aluna "([^\"]*)" realizado por mim$/, async (aluna) => {
+        var n;
+        n = document.getElementsByName("aNota");
+        n == aluna;
+    });
+    
+
+    Given(/^o campo acesso está marcado$/, async () => {
+        await browser.get("http://localhost:4200/");
+        await $("a[name='fb']").click();
+        await $("input[name='checktest']").click();
+    });
+
+    Given(/^o monitor Mateus realizou o feedback da submissão 1 da aluna Maria$/, async () => {
+        await $("button[name='gerar']").click();
+    });
+    Given(/^o monitor André realizou o feedback da submissão 1 do aluno Manoel$/, async () => {
+    });
+    When(/^Eu abro a tela de Feedbacks$/, async () => {
+    });
+    Then(/^Eu posso visualizar todos feedback realizados por todos os monitores$/, async () => {
+    });
+    
 });
+
